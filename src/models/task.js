e8e0f85d7fbb8e5e70f10fb7ad70f8cae71b4f58
Tiago Ferreira <sikozonbatata@gmail.com>
2019-07-13 17:05:38 +0100

    Revert "Created -> Task and User Models"
    This reverts commit 7303718c524062ebccb46c9d2bed777b1d52880c.

--- src/models/task.js
+++ src/models/task.js
@@ -2,28 +2,4 @@ const mongoose = require("mongoose")
 const validator = require("validator")
 
 
-const taskSchema = new moongoseSchema({
-    description: {
-        type: String,
-        trim: true,
-        required: true,
-        validade(value) {
-            if (validator.isInt(value)) {
-                throw new Error("Your description cannot be only a number")
-            }
-        },
-        completed: {
-            type: Boolean,
-            default: false
-        }
-
-    }
-
-}, {
-    timestamps: true
-})
-
-
-const Task = mongoose.model("Task", taskSchema)
-
-module.exports = Task
+// const Task = new moongoseSchema
