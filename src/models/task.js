7303718c524062ebccb46c9d2bed777b1d52880c
Paulo Rigter <paulorigter@gmail.com>
2019-07-13 12:43:28 +0100

    Created -> Task and User Models

--- src/models/task.js
+++ src/models/task.js
@@ -2,4 +2,28 @@ const mongoose = require("mongoose")
 const validator = require("validator")
 
 
-// const Task = new moongoseSchema
+const taskSchema = new moongoseSchema({
+    description: {
+        type: String,
+        trim: true,
+        required: true,
+        validade(value) {
+            if (validator.isInt(value)) {
+                throw new Error("Your description cannot be only a number")
+            }
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
+
+    }
+
+}, {
+    timestamps: true
+})
+
+
+const Task = mongoose.model("Task", taskSchema)
+
+module.exports = Task
