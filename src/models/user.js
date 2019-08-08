827c515d4118d702a2954cc07872e2b37d73f251
Paulo Rigter <paulorigter@gmail.com>
2019-08-08 18:42:43 +0100

    Added User Schema Middleware - To Delete all User Tasks, BEFORE the User itself is deleted

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const validator = require("validator")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const Task = require("./task")
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -122,6 +123,17 @@ userSchema.pre("save", async function (next) {
         user.password = await bcrypt.hash(user.password, 8)
     }
 
+    next()
+})
+
+
+
+// Middleware --> Deletes All User Tasks, BEFORE delete user
+userSchema.pre("remove", async function (next) {
+    const user = this
+
+    await Task.deleteMany({ owner: user._id })
+    
     next()
 })
 
